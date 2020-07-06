4160610cc77f6d37af1045c0f831cbb6765b83b1
WeendyPalomo <58180852+WeendyPalomo@users.noreply.github.com>
2020-07-06 10:55:29 -0500

    Update SumOddsRange.java

--- SumOddsRange.java
+++ SumOddsRange.java
@@ -24,15 +24,22 @@ Ejemplo de entrada/salida:
 #2: 6
 8
 */
-class SumOddsRange {
-  public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
-    System.out.print("#1: ");
-    int num1 = s.nextInt();
-    System.out.print("#2: ");
-    int num2 = s.nextInt();
-    //start below
-    
-    
-  }
+public class SumOddsRange {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("#1: ");
+        int num1 = s.nextInt();
+        System.out.print("#2: ");
+        int num2 = s.nextInt();
+        int sum = 0;
+        for (int i = num1; i <= num2; i++) {
+            if (i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
 }
+
+
